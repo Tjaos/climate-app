b5ecb74b14aa6e6e1aca44a4364d1350ea3a6051
tjaos <th83287716@gmail.com>
2023-10-27 10:58:45 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Main from './screens/main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Main/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    padding:0,
     justifyContent: 'center',
   },
 });
