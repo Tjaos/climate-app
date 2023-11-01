d8e3ccb131988d269174ab49e268b36f719c7af1
tjaos <th83287716@gmail.com>
2023-11-01 15:17:32 -0300

    layout ended

--- screens/Main.js
+++ screens/Main.js
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { Image } from "react-native-elements";
+import { SelectList } from "react-native-dropdown-select-list";
+import { useState } from "react";
+import { ScrollView } from "react-native";
 
 export default function Main() {
+
+  const [selected, setSelected ] = useState("");
+
+  const data = [
+    {key:'1', value:'Recife'},
+    {key:'2', value:'Jaboatão dos Guararapes'},
+    {key:'3', value:'Gravatá'},
+    {key:'4', value:'Caruaru'},
+    {key:'5', value:'João Pessoa'},
+    {key:'6', value:'Garanhuns'},
+    {key:'7', value:'Igarassu'},
+
+  ]
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.background}>
         <View style={styles.topContent}>
           <View
@@ -20,9 +36,11 @@ export default function Main() {
               color={"rgb(103,113,167)"}
               style={{ paddingLeft: 10 }}
             />
-            <Text style={{ fontSize: 14, fontWeight: "bold" }}>
-              Localização
-            </Text>
+            <SelectList style={{ fontSize: 14, fontWeight: "bold" }}
+              setSelected={(val => setSelected(val))}
+              data={data}
+              save="value"
+            />
           </View>
           <View>
             <Icon
@@ -38,7 +56,7 @@ export default function Main() {
             style={{ height: 100, width: 100 }}
             source={require("../assets/cloud.png")}
           />
-          <Text style={{ fontSize: 80, fontWeight: "bold", color: "#fff" }}>
+          <Text style={{ fontSize: 60, fontWeight: "400", color: "#fff" }}>
             26°
           </Text>
           <Text style={{ color: "#fff", fontSize: 16 }}>precipitacions</Text>
@@ -150,18 +168,58 @@ export default function Main() {
                 
               </View>
             </View>
+
           </View>
+          <View style={{
+              marginTop: 20,
+              marginBottom:20,
+              width: "90%",
+              height: 200,
+              backgroundColor: "#5F9F9F",
+              borderRadius: 23,
+            }}>
+              <View
+              style={{
+                justifyContent: "space-between",
+                flexDirection: "column",
+                padding: 15,
+              }}
+            >
+              <View
+                style={{
+                  justifyContent: "space-between",
+                  flexDirection: "row",
+                }}
+              >
+                <Text style={{ fontWeight: "bold", color: "#fff" }}>Next Forecast</Text>
+                <Icon name="event" size={25} color={'#fff'} />
+              </View>
+              <View 
+              style={{
+                justifyContent:"space-between",
+                flexDirection:"row",
+                paddingTop:20
+                }}>
+                  <Text style={{color:'#fff'}}>Monday</Text>
+                  <Icon name="grain" size={25} color={'#fff'} />
+                  <Text style={{color:'#fff'}}>13°c  <Text style={{color:'#c0c0c0'}} >10°c</Text></Text>
+               
+                
+              </View>
+            </View>
+          </View>
+
+          
         </View>
       </View>
-    </View>
+      
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 0,
-    height: "100%",
+    paddingTop: 1,
     width: "100%",
   },
   topContent: {
@@ -171,7 +229,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   midContent: {
-    marginTop: "10%",
+    marginTop: "20%",
     height: "30%",
     alignItems: "center",
     justifyContent: "center",
@@ -179,7 +237,7 @@ const styles = StyleSheet.create({
   },
   botContent: {
     flexDirection: "column",
-    marginTop: 10,
+    paddingTop: 30,
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 15,
